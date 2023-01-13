e3a9b204607d4ac5541693f1de094257e3df1d55
Michael Savin <jtprogru@gmail.com>
2023-01-13 14:16:05 +0300

    update: Add task0001

---
+++ internal/task0001/task0001.go
@@ -0,0 +1,27 @@
+// Что выведет следующая программа и почему?
+// Сделай так, чтобы тест не падал
+package task0001
+
+type Person struct {
+	Name string
+}
+
+type Resp struct {
+	First  *Person
+	Second *Person
+}
+
+func changeName(person *Person) {
+	person = &Person{
+		Name: "Alice",
+	}
+}
+
+func Solution(origin *Person) Resp {
+	var resp = Resp{}
+	resp.First = origin
+	resp.Second = &Person{Name: "John"}
+
+	changeName(resp.First)
+	return resp
+}
---
+++ internal/task0001/task0001_test.go
@@ -0,0 +1,35 @@
+package task0001
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTodoListPostgres_Create(t *testing.T) {
+	tests := []struct {
+		name   string
+		person *Person
+		whant  Resp
+	}{
+		{
+			name:   "First",
+			person: &Person{Name: "Bob"},
+			whant: Resp{
+				First: &Person{
+					Name: "Alice",
+				},
+				Second: &Person{
+					Name: "John",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := Solution(tt.person)
+			assert.Equal(t, res, tt.whant)
+		})
+	}
+}
