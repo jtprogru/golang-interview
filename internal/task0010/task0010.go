5aafe5e3beea7c734f7b13e132ba573085d4ad4f
Michael Savin <jtprogru@gmail.com>
2023-01-16 20:32:48 +0300

    update: Add task0010

---
+++ internal/task0010/task0010.go
@@ -0,0 +1,9 @@
+/*
+https://leetcode.com/problems/insert-interval/
+*/
+
+package task0010
+
+func Solution(intervals [][]int, newInterval []int) [][]int {
+	panic("need to implement...")
+}
---
+++ internal/task0010/task0010_test.go
@@ -0,0 +1,36 @@
+package task0010
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTask0010Solution(t *testing.T) {
+	tests := []struct {
+		name        string
+		intervals   [][]int
+		newInterval []int
+		whant       [][]int
+	}{
+		{
+			name:        "example 1",
+			intervals:   [][]int{{1, 3}, {6, 9}},
+			newInterval: []int{2, 5},
+			whant:       [][]int{{1, 5}, {6, 9}},
+		},
+		{
+			name:        "example 2",
+			intervals:   [][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}},
+			newInterval: []int{4, 8},
+			whant:       [][]int{{1, 2}, {3, 10}, {12, 16}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := Solution(tt.intervals, tt.newInterval)
+			assert.Equal(t, res, tt.whant)
+		})
+	}
+}
