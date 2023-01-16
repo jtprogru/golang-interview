90d6e3f5543ddd0606de0068081aae8ac94ae574
Michael Savin <jtprogru@gmail.com>
2023-01-16 16:25:18 +0300

    fix: remove implementation for task0006

--- internal/task0006/task0006.go
+++ internal/task0006/task0006.go
@@ -7,42 +7,9 @@ package task0006
 type Resp [3]uint64
 
 func Solution(prod uint64) Resp {
-	var (
-		resp                 = Resp{}
-		current_value uint64 = 0
-		x             uint64 = 0
-		fib1          uint64 = 0
-		fib2          uint64 = 0
-	)
-	for current_value < prod {
-		x += 1
-		fib1 = fib(x)
-		fib2 = fib(x + 1)
-		current_value = fib1 * fib2
-	}
-	if current_value == prod {
-		resp[0] = fib1
-		resp[1] = fib2
-		resp[2] = 1
-		return resp
-
-	}
-
-	resp[0] = fib1
-	resp[1] = fib2
-	resp[2] = 0
-	return resp
+	panic("need to implement...")
 }
 
 func fib(n uint64) uint64 {
-	var (
-		a uint64 = 1
-		b uint64 = 1
-		i uint64 = 0
-	)
-
-	for i = 0; i < n-1; i++ {
-		a, b = b, a+b
-	}
-	return a
+	panic("need to implement...")
 }
