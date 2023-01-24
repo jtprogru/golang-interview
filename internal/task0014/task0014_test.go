d057000e3b1d0ca7113585fff063155dd19781d4
Michael Savin <jtprogru@gmail.com>
2023-01-24 22:50:06 +0300

    update: Add task0014

---
+++ internal/task0014/task0014_test.go
@@ -0,0 +1,135 @@
+package task0014
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTask0014Solution(t *testing.T) {
+	tests := []struct {
+		name  string
+		l1    *ListNode
+		l2    *ListNode
+		whant *ListNode
+	}{
+		{
+			name: "example 1",
+			l1: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  3,
+						Next: nil,
+					},
+				},
+			},
+			l2: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 6,
+					Next: &ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+			whant: &ListNode{
+				Val: 7,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val:  8,
+						Next: nil,
+					},
+				},
+			},
+		},
+		{
+			name: "example 2",
+			l1: &ListNode{
+				Val:  0,
+				Next: nil,
+			},
+			l2: &ListNode{
+				Val:  0,
+				Next: nil,
+			},
+			whant: &ListNode{
+				Val:  0,
+				Next: nil,
+			},
+		},
+		{
+			name: "example 3",
+			l1: &ListNode{
+				Val: 9,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 9,
+								Next: &ListNode{
+									Val: 9,
+									Next: &ListNode{
+										Val:  9,
+										Next: nil,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			l2: &ListNode{
+				Val: 9,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val:  9,
+							Next: nil,
+						},
+					},
+				},
+			},
+			whant: &ListNode{
+				Val: 8,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 0,
+								Next: &ListNode{
+									Val: 0,
+									Next: &ListNode{
+										Val: 0,
+										Next: &ListNode{
+											Val:  1,
+											Next: nil,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := Solution(tt.l1, tt.l2)
+			assert.Equal(t, res, tt.whant)
+		})
+	}
+}
