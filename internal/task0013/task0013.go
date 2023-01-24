e2dbfff41f71a216657cae98657c78273702543a
Michael Savin <jtprogru@gmail.com>
2023-01-24 22:33:01 +0300

    fix: replace return to panic

--- internal/task0013/task0013.go
+++ internal/task0013/task0013.go
@@ -1,7 +1,10 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they
+add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly
+one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
 */
@@ -9,5 +12,5 @@ You can return the answer in any order.
 package task0013
 
 func Solution(nums []int, target int) []int {
-	return []int{}
+	panic("need to implement...")
 }
