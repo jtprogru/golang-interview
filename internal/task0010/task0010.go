1d9df9ca9abfa1b16b809390a58e9794adf436ba
Michael Savin <jtprogru@gmail.com>
2023-05-08 23:20:26 +0300

    fix: Refactoring task0010
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- internal/task0010/task0010.go
+++ internal/task0010/task0010.go
@@ -4,6 +4,60 @@ https://leetcode.com/problems/insert-interval/
 
 package task0010
 
+type Interval struct {
+	Start int
+	End   int
+}
+
 func Solution(intervals [][]int, newInterval []int) [][]int {
-	panic("need to implement...")
+	ilist := []Interval{}
+	for _, v := range intervals {
+		tmp := Interval{v[0], v[1]}
+		ilist = append(ilist, tmp)
+	}
+	newItv := Interval{newInterval[0], newInterval[1]}
+	resList := insertIntervals(ilist, newItv)
+	res := [][]int{}
+	for _, v := range resList {
+		tmp := []int{v.Start, v.End}
+		res = append(res, tmp)
+	}
+	return res
+}
+
+func insertIntervals(a []Interval, newInterval Interval) []Interval {
+	res := []Interval{}
+	if len(a) == 0 {
+		res = append(res, newInterval)
+		return res
+	}
+	cur := 0
+	for cur < len(a) && a[cur].End < newInterval.Start {
+		res = append(res, a[cur])
+		cur++
+	}
+	for cur < len(a) && a[cur].Start <= newInterval.End {
+		newInterval = Interval{Start: min(newInterval.Start, a[cur].Start), End: max(newInterval.End, a[cur].End)}
+		cur++
+	}
+	res = append(res, newInterval)
+	for cur < len(a) {
+		res = append(res, a[cur])
+		cur++
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
