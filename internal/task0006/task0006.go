c3c3602e9921c224cb27fb3d9788c01d55dc1ba0
Michael Savin <jtprogru@gmail.com>
2023-05-08 22:49:18 +0300

    fix: Refactoring task0006
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- internal/task0006/task0006.go
+++ internal/task0006/task0006.go
@@ -7,9 +7,12 @@ package task0006
 type Resp [3]uint64
 
 func Solution(prod uint64) Resp {
-	panic("need to implement...")
-}
-
-func fib(n uint64) uint64 {
-	panic("need to implement...")
+	var f1, f2 uint64 = 0, 1
+	for f1*f2 < prod {
+		f1, f2 = f2, f1+f2
+	}
+	if f1*f2 == prod {
+		return [3]uint64{f1, f2, 1}
+	}
+	return [3]uint64{f1, f2, 0}
 }
