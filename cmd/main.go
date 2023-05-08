be057179fe62e7dcb162da57dbc7391a7777e51e
Michael Savin <jtprogru@gmail.com>
2023-05-08 23:35:19 +0300

    upd: Refactoring main
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("Please run go test for details")
+	err := runTests("./internal/...")
+	if err != nil {
+		fmt.Println("Тестирование завершилось с ошибками:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Тестирование успешно завершено")
+	// Здесь ваш код для основной логики программы
+	fmt.Println("Для более подробного вывода запускать:")
+	fmt.Println("\tmake testalltasks")
+}
+
+func runTests(pattern string) error {
+	cmd := exec.Command("go", "test", pattern)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("ошибка при запуске тестов: %w", err)
+	}
+	return nil
 }
